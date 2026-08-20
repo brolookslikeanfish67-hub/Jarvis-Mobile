87cdbb69ec7e65f029008689952a7026ac9c8c4f
ryen stuff <brolookslikeanfish67@yahoo.com>
2026-08-20 12:47:50 -0400

    Add buildscript dependencies for Android and Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// build.gradle.kts (Project)
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
